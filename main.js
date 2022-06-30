8b33b0f1f9ccfd8109bb4dd0a3c04627d2157def
POOM <nitipoom_t@cmu.ac.th>
2022-06-30 23:28:33 +0700

    5.3

--- main.js
+++ main.js
@@ -9,7 +9,11 @@ const app = Vue.createApp({
             inStock: true,
             onSale: true,
             inventory: 100,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+            ]
         }
     }
 })
