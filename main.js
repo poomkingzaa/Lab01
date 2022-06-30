95b0006df2245d13c45948674666da922f9b5d6f
POOM <nitipoom_t@cmu.ac.th>
2022-07-01 00:11:49 +0700

    decorate

--- main.js
+++ main.js
@@ -2,14 +2,15 @@ const product = 'Socks'
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Shoes',
-            info: 'test',
+            product: 'Sock',
+            info: 'This sock is can use in everyday life.',
             image: './assets/images/socks_green.jpg',
             web: 'https://www.camt.cmu.ac.th/index.php/th/',
             inStock: true,
             onSale: true,
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
+            select: "Select Color",
             variants: [
                 { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
                 { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
