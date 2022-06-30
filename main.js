5b160ef4ebdfc91d5fdd2a299c094d363105753a
POOM <nitipoom_t@cmu.ac.th>
2022-06-30 22:58:38 +0700

    4.2

--- main.js
+++ main.js
@@ -2,10 +2,11 @@ const product = 'Socks'
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Boots',
+            product: 'Shoes',
             info: 'test',
             image: './assets/images/socks_green.jpg',
-            web: 'https://www.camt.cmu.ac.th/index.php/th/'
+            web: 'https://www.camt.cmu.ac.th/index.php/th/',
+            inStock: true
         }
     }
 })
