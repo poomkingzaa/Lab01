ade370cb5da024527950ba012a6eae7a8c08449d
POOM <nitipoom_t@cmu.ac.th>
2022-06-30 23:54:14 +0700

    6.6

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ const app = Vue.createApp({
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
             ],
             size: 'Size = S,M,L',
             cart: 0
@@ -21,6 +21,9 @@ const app = Vue.createApp({
     methods: {
         addToCart() {
             this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 })
